11884f115141407120de240d9e5205350913208b
datsuns <the.skeleton7@gmail.com>
2020-09-13 18:33:45 +0900

    voltsetup.bat: remove vim-go

--- voltsetup.bat
+++ voltsetup.bat
@@ -8,7 +8,6 @@ volt get github.com/vim-jp/cpp-vim
 volt get github.com/tpope/vim-endwise
 volt get github.com/tpope/vim-fugitive
 volt get github.com/thinca/vim-quickrun
-volt get github.com/fatih/vim-go
 volt get github.com/previm/previm
 volt get github.com/tyru/open-browser.vim
 volt get github.com/peterhoeg/vim-qml
@@ -16,11 +15,13 @@ volt get github.com/dart-lang/dart-vim-plugin
 volt get github.com/junegunn/fzf
 volt get github.com/junegunn/fzf.vim
 volt get github.com/PProvost/vim-ps1
+volt get github.com/prabirshrestha/async.vim
 volt get github.com/prabirshrestha/asyncomplete.vim
 volt get github.com/prabirshrestha/asyncomplete-lsp.vim
 volt get github.com/prabirshrestha/vim-lsp
 volt get github.com/mattn/vim-lsp-settings
 volt get github.com/mattn/vim-lsp-icons
+volt get github.com/mattn/vim-goimports
 volt get github.com/hrsh7th/vim-vsnip
 volt get github.com/hrsh7th/vim-vsnip-integ
 volt get https://github.com/ziglang/zig.vim
@@ -29,6 +30,7 @@ volt get github.com/datsuns/vim-go-openimport
 volt get github.com/datsuns/astyle.vim
 volt get github.com/datsuns/vim-flutter
 
+rem volt get github.com/fatih/vim-go
 rem volt get github.com/Shougo/vimproc.vim
 rem volt get github.com/cespare/vim-toml
 rem volt get github.com/thosakwe/vim-flutter
