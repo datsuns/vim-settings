4f5a85291a4bb84acf44d7a810a564a8a83a806d
datsuns <the.skeleton7@gmail.com>
2025-01-30 23:28:02 +0900

    add script to generate compile_flag.txt

---
+++ for_lsp/search.go
@@ -0,0 +1,95 @@
+package main
+
+import (
+	"bytes"
+	"io/fs"
+	"os"
+	"os/exec"
+	"path/filepath"
+	"regexp"
+	"slices"
+	"strings"
+)
+
+const destFilename = "_compile_flags.gen.txt"
+
+var staticOptions = []string{
+	"-std=c++20",
+}
+var headerFileKey = regexp.MustCompile("\\.(h|hxx|hpp)$")
+
+func executeWithDummyInput(tool string, params []string) (string, error) {
+	//fmt.Printf(" >> %v %v\n", tool, params)
+	cmd := exec.Command(tool, params...)
+	cmd.Stdin = bytes.NewBufferString("")
+	log, _ := cmd.CombinedOutput()
+	return string(log), nil
+}
+
+func collectCompilerSeachPaths(dummyFile string) []string {
+	_, err := exec.LookPath("gcc")
+	if err != nil {
+		return []string{}
+	}
+	log, _ := executeWithDummyInput("gcc", []string{"-xc++", "-E", "-v", "-"})
+	body := false
+	lineFeeds := regexp.MustCompile(`[\r\n]`)
+	paths := []string{}
+	for _, line := range strings.Split(log, "\n") {
+		line = lineFeeds.ReplaceAllString(line, "")
+		if strings.Compare(line, "End of search list.") == 0 {
+			break
+		}
+
+		if strings.Compare(line, "#include <...> search starts here:") == 0 {
+			body = true
+			continue
+		}
+
+		if body {
+			paths = append(paths, strings.TrimSpace(filepath.ToSlash(line)))
+		}
+	}
+	return paths
+}
+
+func collectHeaderFileDirs(root string) []string {
+	all := []string{}
+	filepath.WalkDir(root, func(path string, d fs.DirEntry, err error) error {
+		if d.IsDir() {
+			return nil
+		}
+		ext := filepath.Ext(path)
+		if !headerFileKey.MatchString(ext) {
+			return nil
+		}
+		abs, err := filepath.Abs(path)
+		if err != nil {
+			return nil
+		}
+		all = append(all, filepath.ToSlash(filepath.Dir(abs)))
+		return nil
+	})
+	slices.Sort(all)
+	return slices.Compact(all)
+}
+
+func main() {
+	root := os.Args[1]
+	compilerPaths := collectCompilerSeachPaths(os.Args[0])
+	headerPaths := collectHeaderFileDirs(root)
+	dest, err := os.Create(destFilename)
+	if err != nil {
+		panic(err)
+	}
+	defer dest.Close()
+	for _, opt := range staticOptions {
+		dest.WriteString(opt + "\n")
+	}
+	for _, path := range compilerPaths {
+		dest.WriteString("-I" + path + "\n")
+	}
+	for _, path := range headerPaths {
+		dest.WriteString("-I" + path + "\n")
+	}
+}
